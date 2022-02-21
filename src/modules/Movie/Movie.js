280b656d90cb3f3b848b6cc38efaa2733a84cbfd
hyo-choi <hyochoi31@gmail.com>
2022-02-22 01:27:54 +0900

    :recycle: refactor: movieId를 변수로 분리

--- src/modules/Movie/Movie.js
+++ src/modules/Movie/Movie.js
@@ -1,9 +1,10 @@
 import styles from './Movie.module.css';
 
 export default function Movie({ $target }) {
+  const movieId = 'k4lC4xuKv3U';
   const $section = document.createElement('section');
   $section.insertAdjacentHTML('afterbegin', `
-    <iframe src="https://www.youtube.com/embed/k4lC4xuKv3U?&amp;autoplay=1&mute=1&amp;loop=1&controls=0&playlist=k4lC4xuKv3U"
+    <iframe src="https://www.youtube.com/embed/${movieId}?&amp;autoplay=1&mute=1&amp;loop=1&controls=0&playlist=${movieId}"
     title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
     `);
   $target.appendChild($section);
